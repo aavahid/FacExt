edb017d66faf6625dff3544c9bf7bf2432e198a6
vahidaa <vahidaa@code.edu.az>
2023-08-05 13:47:30 +0400

    FacExt created

---
+++ FactorialExt/IntExtention.cs
@@ -0,0 +1,19 @@
+﻿public static class IntExtensions
+{
+    public static int Factorial(this int number)
+    {
+        if (number < 0)
+            throw new ArgumentException("Factorial menfi sayi ola bilmez");
+
+        if (number == 0 || number == 1)
+            return 1;
+
+        int result = 1;
+        for (int i = 2; i <= number; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+}
+
---
+++ FactorialExt/Program.cs
@@ -0,0 +1,22 @@
+﻿int number;
+while (true)
+{
+    Console.Write("Faktorial hesablamaq ucun ededi daxil edin: ");
+    if (int.TryParse(Console.ReadLine(), out number))
+    {
+        try
+        {
+            var result = number.Factorial();
+            Console.WriteLine("Netice: " + result);
+            break;
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Duzgun eded daxil edin.");
+    }
+}
